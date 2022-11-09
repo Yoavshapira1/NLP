3a88f0068e147b738cfaa1c0270e77ba51b9241b
Nadav Fuchs <nadavf95@gmail.com>
2022-11-09 13:06:45 +0200

    skelaton

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,3 +1,24 @@
+import spacy
+from datasets import load_dataset
+
+main_dic = dict()
+
+def process_data_set():
+    nlp = spacy.load("en_core_web_sm")
+    dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
+    i = 0
+    for text in dataset["text"]:
+        doc = nlp(text)
+        print(doc.text)
+        print()
+        i += 1
+        if i == 5:
+            break
+    return nlp
+
+def process_word(word):
+    pass
+
 def increase_counter(dict, val):
     try:
         dict[val] += 1
