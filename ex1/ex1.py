c0615b77b5688a804d0455879d2287c2ad51448b
Nadav Fuchs <nadavf95@gmail.com>
2022-11-11 13:56:29 +0200

    process_data_set implemented!!

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -44,7 +44,7 @@ class Word:
 
     def increase_unigram_counter(self) -> None:
         self.uni_gram_counter += 1
-        if self.word != "START":
+        if self.word != "start":
             self.corpus_size += 1
 
     def increase_bigram_counter(self, word : str) -> None:
@@ -100,32 +100,31 @@ def add_word_to_bi_gram_dict(first, second):
 def process_data_set():
     nlp = spacy.load("en_core_web_sm")
     dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
-
+    i = 0
     for text in dataset["text"]:
+        i+=1
+        if i >50:
+            break
         j = 0
-        print("\n new text")
         doc = nlp(text)
 
         while j < len(doc) and not doc[j].is_alpha:
             j += 1
 
         if j < len(doc):
-            # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    TO DO   insert START
-            pass
-
+            start = nlp('START')[0].lemma_
+            add_word_to_main_dict(start)
+            add_word_to_bi_gram_dict(start, doc[j].lemma_)
 
         while j < len(doc):
-            # to do add_word_to_main_dict(doc[j].lemma)
+            add_word_to_main_dict(doc[j].lemma_)
             k = j + 1
             while k < len(doc):
                 if doc[k].is_alpha:
-                    # to do add_word_to_bi_gram_dict(doc[j].lemma, doc[k].lemma)
-                    print(doc[j].text + ":  " + doc[k].text)
+                    add_word_to_bi_gram_dict(doc[j].lemma_, doc[k].lemma_)
                     break
                 k += 1
             j = k
-
-    print("!!!!!!!!!!!!!!!!!!!!!     EEEEEEEEEEEEEEEEEEEENNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDD !!!!!!!!!!!!!!!!!!!!!!!!!")
 
 def process_word(word):
     pass
@@ -142,40 +141,40 @@ def sentence_to_string_list(str):
     return stripped.split()
 
 if __name__ == "__main__":
-    # Psudo code *after processing*:
+    # # Psudo code *after processing*:
+    # #
+    # # - for every document D:
+    # #     - add "START" at beginning and "STOP" at the end
+    # #     - for every consecutive words W1, W2:
+    # #         add_word_to_main_dict(W1)
+    # #         add_word_to_bi_gram_dict(W1, W2)
+    # # save main_dict as a JSON file!! So we don't need to run this code again
     #
-    # - for every document D:
-    #     - add "START" at beginning and "STOP" at the end
-    #     - for every consecutive words W1, W2:
-    #         add_word_to_main_dict(W1)
-    #         add_word_to_bi_gram_dict(W1, W2)
-    # save main_dict as a JSON file!! So we don't need to run this code again
-
-    # Pipe line example
-    # just a sentence
-    example = "Additionally, suppose you need to customize the functionality of any other standard dictionary method. In that case, you’ll have to override the method at hand and provide a suitable implementation that fulfills your needs. This process implies a fair amount of work. It’s also error-prone and requires advanced knowledge of Python and its data model. It can also imply performance issues because you’ll be writing the class in pure Python. The main advantage of this strategy is that the parent ABC will alert you if you miss any method in your custom implementation. For these reasons, you should embrace this strategy only if you need a dictionary-like class that’s fundamentally different from the built-in dictionary. In this tutorial, you’ll focus on creating dictionary-like classes by inheriting from the built-in dict class and the UserDict class, which seem to be the quickest and most practical strategies. Additionally, suppose you need to customize the functionality of any other standard dictionary method. a ggg In that case, you’ll have to override a ggg the method at hand and provide a ggg implementation that fulfills your needs. This process implies a fair amount of work. It’s also error-prone and requires advanced knowledge of Python and pure pure pure its data model. It can also imply performance issues because you’ll be writing the class in pure Python. The main advantage of this strategy is that the parent ABC will alert you if you miss any method in your custom implementation. For these reasons, you should embrace this strategy only if you need a dictionary-like class that’s fundamentally different from the built-in dictionary."
-    # keep only alphabet words and add START & STOP
-    example = sentence_to_string_list(example)
-    # pipe line
-    for i in range(len(example)-1):
-        W1, W2 = example[i], example[i+1]
-        add_word_to_main_dict(W1)
-        add_word_to_bi_gram_dict(W1, W2)
-
-    # JSON encoding & decoding example
-    # encode
-    with open('main_dict.json', 'w') as fp:
-        json.dump(main_dict, fp, indent=4, cls=WordEncoder)
-
-    # decode
-    with open('main_dict.json', 'r') as fp:
-        data = json.load(fp)
-    for w in data.values():
-        word = Word(w["word"])
-        word.uni_gram_counter = w["uni_gram_counter"]
-        word.bi_gram_counters = w["bi_gram_counters"]
-        word.max_bi_gram_counter = w["max_bi_gram_counter"]
-        word.str_of_max_bi_gram_counter = w["str_of_max_bi_gram_counter"]
-        data[w["word"]] = word
+    # # Pipe line example
+    # # just a sentence
+    # example = "Additionally, suppose you need to customize the functionality of any other standard dictionary method. In that case, you’ll have to override the method at hand and provide a suitable implementation that fulfills your needs. This process implies a fair amount of work. It’s also error-prone and requires advanced knowledge of Python and its data model. It can also imply performance issues because you’ll be writing the class in pure Python. The main advantage of this strategy is that the parent ABC will alert you if you miss any method in your custom implementation. For these reasons, you should embrace this strategy only if you need a dictionary-like class that’s fundamentally different from the built-in dictionary. In this tutorial, you’ll focus on creating dictionary-like classes by inheriting from the built-in dict class and the UserDict class, which seem to be the quickest and most practical strategies. Additionally, suppose you need to customize the functionality of any other standard dictionary method. a ggg In that case, you’ll have to override a ggg the method at hand and provide a ggg implementation that fulfills your needs. This process implies a fair amount of work. It’s also error-prone and requires advanced knowledge of Python and pure pure pure its data model. It can also imply performance issues because you’ll be writing the class in pure Python. The main advantage of this strategy is that the parent ABC will alert you if you miss any method in your custom implementation. For these reasons, you should embrace this strategy only if you need a dictionary-like class that’s fundamentally different from the built-in dictionary."
+    # # keep only alphabet words and add START & STOP
+    # example = sentence_to_string_list(example)
+    # # pipe line
+    # for i in range(len(example)-1):
+    #     W1, W2 = example[i], example[i+1]
+    #     add_word_to_main_dict(W1)
+    #     add_word_to_bi_gram_dict(W1, W2)
+    #
+    # # JSON encoding & decoding example
+    # # encode
+    # with open('main_dict.json', 'w') as fp:
+    #     json.dump(main_dict, fp, indent=4, cls=WordEncoder)
+    #
+    # # decode
+    # with open('main_dict.json', 'r') as fp:
+    #     data = json.load(fp)
+    # for w in data.values():
+    #     word = Word(w["word"])
+    #     word.uni_gram_counter = w["uni_gram_counter"]
+    #     word.bi_gram_counters = w["bi_gram_counters"]
+    #     word.max_bi_gram_counter = w["max_bi_gram_counter"]
+    #     word.str_of_max_bi_gram_counter = w["str_of_max_bi_gram_counter"]
+    #     data[w["word"]] = word
 
     process_data_set()
