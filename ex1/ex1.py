e2d378d701a0cbc826a9446dc6ab6e67159b9abe
Nadav Fuchs <nadavf95@gmail.com>
2022-11-13 17:59:30 +0200

    done

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -8,17 +8,6 @@ nlp = spacy.load("en_core_web_sm")
 
 class Word:
 
-    # Automatically updating the corpus size
-    _corpus_size = 0
-
-    @property
-    def corpus_size(self):
-        return type(self)._corpus_size
-
-    @corpus_size.setter
-    def corpus_size(self, val):
-        type(self)._corpus_size += 1
-
     def __init__(self, word):
         self.word = word
         self.uni_gram_counter = 0
@@ -27,16 +16,21 @@ class Word:
         self.str_of_max_bi_gram_counter = "N.A"
         self.increase_unigram_counter()
 
-    def uni_prob(self) -> float:
-        """ Return the uni-gram probability"""
-        return np.log(self.uni_gram_counter / self._corpus_size)
-
-    def bi_prob(self, word : str) -> float:
-        """ Return the probability of a given string to appear after self.word"""
+    def get_uni_gram_counter(self):
+        # return self.uni_gram_counter / self._corpus_size
+        return self.uni_gram_counter
+    def bi_prob(self, word):
         try:
-            return np.log(self.bi_gram_counters[word] / self.uni_gram_counter)
+            return self.bi_gram_counters[word] / self.uni_gram_counter
         except KeyError:
+            return 0
+
+    def bi_prob_log(self, word : str) -> float:
+        """ Return the probability of a given string to appear after self.word"""
+        p = self.bi_prob(word)
+        if p == 0:
             return -np.inf
+        return np.log(p)
 
     def MLE(self) -> str:
         """ Return the most likelihood word to appear after self.word"""
@@ -44,8 +38,6 @@ class Word:
 
     def increase_unigram_counter(self) -> None:
         self.uni_gram_counter += 1
-        if self.word != "start":
-            self.corpus_size += 1
 
     def increase_bigram_counter(self, word : str) -> None:
         """ Adds a given word to the bigram dictionary, or increase the counters if already exists.
@@ -67,7 +59,7 @@ class Word:
                "Unigram probabilty: %.3f\n" \
                "Most Likey Next Word: %s\n" \
                "MLE probability: %.3f\n\n" % \
-               (self.word, self.uni_gram_counter, self.corpus_size, self.uni_prob(), self.MLE(), self.bi_prob(self.MLE()))
+               (self.word, self.uni_gram_counter, self.corpus_size, self.uni_prob_log(), self.MLE(), self.bi_prob_log(self.MLE()))
 
 
 class WordEncoder(JSONEncoder):
@@ -96,29 +88,29 @@ def increase_bi_gram(first, second):
     main_dict[first].increase_bigram_counter(second)
 
 
-# def process_data_set():
-#     dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
-#     for text in dataset["text"]:
-#         j = 0
-#         doc = nlp(text)
-#
-#         while j < len(doc) and not doc[j].is_alpha:
-#             j += 1
-#
-#         if j < len(doc):
-#             start = nlp('START')[0].lemma_
-#             increase_uni_gram(start)
-#             increase_bi_gram(start, doc[j].lemma_)
-#
-#         while j < len(doc):
-#             increase_uni_gram(doc[j].lemma_)
-#             k = j + 1
-#             while k < len(doc):
-#                 if doc[k].is_alpha:
-#                     increase_bi_gram(doc[j].lemma_, doc[k].lemma_)
-#                     break
-#                 k += 1
-#             j = k
+def process_data_set():
+    dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
+    for text in dataset["text"]:
+        j = 0
+        doc = nlp(text)
+
+        while j < len(doc) and not doc[j].is_alpha:
+            j += 1
+
+        if j < len(doc):
+            start = nlp('START')[0].lemma_
+            increase_uni_gram(start)
+            increase_bi_gram(start, doc[j].lemma_)
+
+        while j < len(doc):
+            increase_uni_gram(doc[j].lemma_)
+            k = j + 1
+            while k < len(doc):
+                if doc[k].is_alpha:
+                    increase_bi_gram(doc[j].lemma_, doc[k].lemma_)
+                    break
+                k += 1
+            j = k
 
 
 def increase_counter(dict, val):
@@ -163,9 +155,10 @@ if __name__ == "__main__":
     #         add_word_to_main_dict(W1)
     #         add_word_to_bi_gram_dict(W1, W2)
 
-    # process_data_set()
+    process_data_set()
     # save_data(main_dict)
-    data = load_data("data.json")
+    # data = load_data("data.json")
+    data = main_dict
 
     sentences = ["Brad Pitt was born in Oklahoma", "The actor was born in USA"]
     M = 0
@@ -173,28 +166,31 @@ if __name__ == "__main__":
     print("Q U E S T I O N    3")
     perp_pow = 0
     for s in sentences:
-        doc = nlp("START" + s)
+        doc = nlp("START " + s)
         prob = 0
         for i in range(len(doc) - 1):
-            prob += data[doc[i].lemma_].bi_prob(doc[i + 1].lemma_)
+            prob += data[doc[i].lemma_].bi_prob_log(doc[i + 1].lemma_)
         perp_pow += prob
-        M += len(s)
+        M += len(s.split())
         print("Probability of the sentence: %s, is %f" % (s, prob))
     perplexity = np.power(np.e, -(perp_pow / M))
     print("The perplexity: %f" % perplexity)
 
     # Q4
     print("Q U E S T I O N    4")
+    corpus_size = 0
+    for val in data.values():
+        if val.word != "start":
+            corpus_size += val.uni_gram_counter
+
     perp_pow = 0
     for s in sentences:
-        doc = nlp("START" + s)
+        doc = nlp("START " + s)
         prob = 0
         for i in range(len(doc) - 1):
-            bi_gram_prob = data[doc[i].lemma_].bi_prob(doc[i + 1].lemma_)
-            if bi_gram_prob > np.inf:
-                prob += 2/3 * bi_gram_prob
-            else:
-                prob += 1/3 * data[doc[i + 1].lemma_].uni_prob()
+            bi_p = data[doc[i].lemma_].bi_prob(doc[i + 1].lemma_)
+            uni_p = data[doc[i + 1].lemma_].get_uni_gram_counter() / corpus_size
+            prob += np.log(((1/3) * uni_p) + ((2/3)*bi_p))
         perp_pow += prob
         print("Probability of the sentence: %s, is %f" % (s, prob))
     perplexity = np.power(np.e, -(perp_pow / M))
