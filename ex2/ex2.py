b5a8806eb30a581557d3ffaeafabd053c6fc45f1
Yoavshapira1 <62549504+Yoavshapira1@users.noreply.github.com>
2022-11-20 16:34:21 +0200

    ex2 stareted

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -5,11 +5,12 @@ nltk.download('brown')
 # TODO: Yoav: Ojbects, save & load data, Viterbi
 # TODO NADAVS: prepare training set
 
-def increase_counter(dict, val):
+def increase_uni_gram(dict, word):
+    """ Increments the counter of a single word to a given dictionary"""
     try:
-        dict[val] += 1
-    except:
-        dict[val] = 1
+        dict[word].increase_unigram_counter()
+    except KeyError:
+        dict[word] = Word(word)
 
 
 if __name__ == "__main__":
@@ -19,11 +20,10 @@ if __name__ == "__main__":
     tag_sent = [str(i % 3) for i in range(len(sent))]
 
     for word, tag in zip(sent, tag_sent):
-        increase_counter(words, word)
-        increase_counter(tags, tag)
-
+        increase_uni_gram(words, word)
+        increase_uni_gram(tags, tag)
         # add tag to Word
         words[word].increase_bigram_counter(tag)
 
         # add prev tag to Tag
-        tags[tag].increase_bigram_counter(tag-1)
+        tags[tag].increase_bigram_counter(tag+1)
